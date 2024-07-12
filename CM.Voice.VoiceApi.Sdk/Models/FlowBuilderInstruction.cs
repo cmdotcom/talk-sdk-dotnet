0901b7f3a8189cea6d820450ee2389faf0f0c83a
Farid <faridamali93@gmail.com>
2024-07-12 15:44:44 +0200

    Added FlowBuilder instruction

---
+++ CM.Voice.VoiceApi.Sdk/Models/FlowBuilderInstruction.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections.Generic;
+using Newtonsoft.Json;
+
+namespace CM.Voice.VoiceApi.Sdk.Models.Instructions.Apps
+{
+    /// <summary>
+    /// This sends the instruction to make and outbound call and start a FlowBuilder flow.
+    /// </summary>
+    public class FlowBuilderInstruction : BaseAppInstruction
+    {
+        /// <summary>
+        /// The unique identifier of the FlowBuilder flow.
+        /// </summary>
+        [JsonProperty("callflow-id", Order = 7)]
+        public string CallflowId { get; set; }
+        
+        /// <summary>
+        /// Optional list of flow variables that will be set.
+        /// </summary>
+        [JsonProperty("flow-variables", Order = 8)]
+        public Dictionary<string, object> FlowVariables { get; set; }
+        
+        /// <summary>
+        /// Determines the Voicemail detection flow.
+        /// </summary>
+        [JsonProperty("voicemail-response", Order = 9)]
+        public VoicemailResponse VoicemailResponse { get; set; } = VoicemailResponse.Stop;
+    }
+}
